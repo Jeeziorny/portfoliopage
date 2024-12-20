edbcca686aedbdc6c5b78d9a337cec3e1c2b4a57
Kamil Jeziorny <kamij98@gmail.com>
2024-12-20 11:11:24 +0100

    init

---
+++ app/not-found.jsx
@@ -0,0 +1,58 @@
+import Link from "next/link";
+
+export const metadata = {
+  title: "404 | Page not found",
+};
+const NotFound = () => {
+  return (
+    <>
+      {/* End page title for seo */}
+
+      <div className="not-found-wrapper">
+        <div className="leftpart">
+          <div className="leftpart_inner">
+            {/* END LOGO */}
+            <div className="copyright">
+              <p>
+                &copy; 2024 Kamil Jeziorny
+              </p>
+            </div>
+            {/* END COPYRIGHT */}
+          </div>
+        </div>
+        {/* END LEFT PART */}
+
+        <div className="rightpart">
+          <div className="rightpart_in">
+            <div className="tokyo_tm_section">
+              <div className="container">
+                <div className="tokyo_tm_error">
+                  <div className="tokyo_tm_error_inner">
+                    <h1>404!</h1>
+                    <h3>Page not found</h3>
+                    <p>The page you were looking for could not be found.</p>
+                    <Link href="/" className="ib-button">
+                      Go to Home
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* END RIGHT PART */}
+
+        <footer className="footer-wrapper">
+          <div className="copyright">
+            <p>
+              &copy; 2024 Kamil Jeziorny
+            </p>
+          </div>
+          {/* END COPYRIGHT */}
+        </footer>
+      </div>
+    </>
+  );
+};
+
+export default NotFound;
